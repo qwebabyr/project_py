69611c932796f481d1a29c416cffd513d202c032
qwebabyr <skoptsova.anuta@yandex.com>
2026-04-30 22:49:35 +0300

    добавила новые команды: list, total, add

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ data_file = "ispenses_data.json"
 
 def load_data():
     if not os.path.exists(data_file):
-        return {"categories":[]}
+        return {"categories":[], "expenses":[]}
     
     with open(data_file, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -22,8 +22,8 @@ def save_data(data):
 def main():
     try:
         data = load_data()
-        if len(sys.argv) <= 2:
-            print("Usage: python3 interest.py inputcat <new category>")
+        if len(sys.argv) < 2:
+            print("Usage: python3 main.py command [arguments]")
             return
 
         command = sys.argv[1]
@@ -35,18 +35,48 @@ def main():
                 data["categories"].append(new_cat)
                 save_data(data)
                 print(f"New category {new_cat} added")
-
-            elif not new_cat:
-                print("Error: name category cannot be empty")
-
             else:
                 print("This category already there")
+                
+        elif command == "add":
+            amount = float(sys.argv[2])
+            category = sys.argv[3]
+            description = " ".join(sys.argv[4:]) 
+
+            if category not in data["categories"]:
+                print(f"Error: Category '{category}' not in file. Please, add.")
+                return
+
+            data["expenses"].append({
+                "amount": amount,
+                "category": category,
+                "description": description
+            })
+            save_data(data)
+            print(f"Rashod '{description}' for the summa {amount} add.")
+        
+        elif command == "list":
+            category_filter = sys.argv[2] if len(sys.argv) > 2 else None
+            print(f"{'Name':<20} | {'Summa':<10} | {'Category':<15}")
+            print("-" * 50)
+
+            for exp in data["expenses"]:
+                if not category_filter or exp["category"] == category_filter:
+                    print(f"{exp['description']:<20} | {exp['amount']:<10} | {exp['category']:<15}")
+                    
+        elif command == "total":
+            category_filter = sys.argv[2] if len(sys.argv) > 2 else None
+            total = sum(exp["amount"] for exp in data["expenses"]
+                        if not category_filter or exp["category"] == category_filter)
+
+            scope = f"in category '{category_filter}'" if category_filter else "all"
+            print(f"Total expenses {scope}: {total}")
 
         else:
             print(f"Unknown command: {command}")
         
     except (IndexError, ValueError):
-        print("Ошибка: неверный ввод аргументов, проверьте формат команд.")
+        print("Error: Check this out.")
 
 
 if __name__ == "__main__":
